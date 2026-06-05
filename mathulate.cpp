201041bacd3993c7c3248e1c746f11b8b962f7b8
Neal Sanche <thorinside@gmail.com>
2026-06-04 19:54:26 -0600

    Add clocked phase transport

--- mathulate.cpp
+++ mathulate.cpp
@@ -8,16 +8,30 @@
 
 #include <distingnt/api.h>
 #include <cstddef>
+#include <stdint.h>
 #include <new>
 
 #include "mathulate_core.h"
 
 namespace {
 
+static const int kDefaultSpeed = 4;
+static const float kClockThreshold = 1.0f;
+static const float kFallbackSampleRate = 48000.0f;
+
 struct MathulateDTC {
     mathulate::State core;
     float outputScale;
     float outputOffset;
+    float phaseOffset;
+    float speedMultiplier;
+    float phase;
+    float displayPhase;
+    float displayOutput;
+    float clockPeriodSamples;
+    uint32_t samplesSinceClock;
+    bool previousClockHigh;
+    bool clockSeen;
 };
 
 struct MathulateAlgorithm : public _NT_algorithm {
@@ -33,6 +47,9 @@ enum {
     kParamEquation,
     kParamScale,
     kParamOffset,
+    kParamClockIn,
+    kParamSpeed,
+    kParamPhaseOffset,
 };
 
 static const char* const equationStrings[] = {
@@ -59,6 +76,31 @@ static const char* const equationStrings[] = {
     NULL,
 };
 
+static const char* const speedStrings[] = {
+    "/16",
+    "/8",
+    "/4",
+    "/2",
+    "x1",
+    "x2",
+    "x3",
+    "x4",
+    "x8",
+    NULL,
+};
+
+static const float speedMultipliers[] = {
+    1.0f / 16.0f,
+    1.0f / 8.0f,
+    1.0f / 4.0f,
+    1.0f / 2.0f,
+    1.0f,
+    2.0f,
+    3.0f,
+    4.0f,
+    8.0f,
+};
+
 static const _NT_parameter parameters[] = {
     NT_PARAMETER_CV_INPUT("CV A", 1, 1)
     NT_PARAMETER_CV_INPUT("CV B", 1, 2)
@@ -66,10 +108,13 @@ static const _NT_parameter parameters[] = {
     { .name = "Equation", .min = 0, .max = mathulate::kNumEquations - 1, .def = mathulate::kEquationAdd, .unit = kNT_unitEnum, .scaling = kNT_scalingNone, .enumStrings = equationStrings },
     { .name = "Scale", .min = 0, .max = 200, .def = 100, .unit = kNT_unitPercent, .scaling = kNT_scalingNone, .enumStrings = NULL },
     { .name = "Offset", .min = -10000, .max = 10000, .def = 0, .unit = kNT_unitVolts, .scaling = kNT_scaling1000, .enumStrings = NULL },
+    NT_PARAMETER_CV_INPUT("Clock In", 0, 0)
+    { .name = "Speed", .min = 0, .max = ARRAY_SIZE(speedMultipliers) - 1, .def = kDefaultSpeed, .unit = kNT_unitEnum, .scaling = kNT_scalingNone, .enumStrings = speedStrings },
+    { .name = "Phase", .min = 0, .max = 100, .def = 0, .unit = kNT_unitPercent, .scaling = kNT_scalingNone, .enumStrings = NULL },
 };
 
-static const uint8_t pageMain[] = { kParamEquation, kParamScale, kParamOffset };
-static const uint8_t pageRouting[] = { kParamInputA, kParamInputB, kParamOutput, kParamOutputMode };
+static const uint8_t pageMain[] = { kParamEquation, kParamSpeed, kParamScale, kParamOffset, kParamPhaseOffset };
+static const uint8_t pageRouting[] = { kParamInputA, kParamInputB, kParamClockIn, kParamOutput, kParamOutputMode };
 
 static const _NT_parameterPage pages[] = {
     { .name = "Main", .numParams = ARRAY_SIZE(pageMain), .group = 0, .unused = { 0, 0 }, .params = pageMain },
@@ -80,6 +125,41 @@ static const _NT_parameterPages parameterPages = {
     .numPages = ARRAY_SIZE(pages),
     .pages = pages,
 };
+
+static float clampf(float x, float lo, float hi) {
+    return x < lo ? lo : (x > hi ? hi : x);
+}
+
+static float speedMultiplierForIndex(int index) {
+    if (index < 0)
+        index = 0;
+    if (index >= (int)ARRAY_SIZE(speedMultipliers))
+        index = ARRAY_SIZE(speedMultipliers) - 1;
+    return speedMultipliers[index];
+}
+
+static float sampleRate() {
+    return NT_globals.sampleRate > 0 ? (float)NT_globals.sampleRate : kFallbackSampleRate;
+}
+
+static void resetClock(MathulateDTC* dtc) {
+    dtc->clockPeriodSamples = sampleRate();
+    dtc->samplesSinceClock = 0;
+    dtc->previousClockHigh = false;
+    dtc->clockSeen = false;
+}
+
+static void initialiseDtc(MathulateDTC* dtc) {
+    mathulate::resetState(&dtc->core);
+    dtc->outputScale = 1.0f;
+    dtc->outputOffset = 0.0f;
+    dtc->phaseOffset = 0.0f;
+    dtc->speedMultiplier = speedMultiplierForIndex(kDefaultSpeed);
+    dtc->phase = 0.0f;
+    dtc->displayPhase = 0.0f;
+    dtc->displayOutput = 0.0f;
+    resetClock(dtc);
+}
 
 static void calculateRequirements(_NT_algorithmRequirements& req, const int32_t*) {
     req.numParameters = ARRAY_SIZE(parameters);
@@ -95,11 +175,7 @@ static _NT_algorithm* construct(const _NT_algorithmMemoryPtrs& ptrs,
     MathulateAlgorithm* alg = new (ptrs.sram) MathulateAlgorithm((MathulateDTC*)ptrs.dtc);
     alg->parameters = parameters;
     alg->parameterPages = &parameterPages;
-
-    mathulate::resetState(&alg->dtc->core);
-    alg->dtc->outputScale = 1.0f;
-    alg->dtc->outputOffset = 0.0f;
-
+    initialiseDtc(alg->dtc);
     return alg;
 }
 
@@ -110,6 +186,12 @@ static void parameterChanged(_NT_algorithm* self, int p) {
         pThis->dtc->outputScale = pThis->v[kParamScale] * 0.01f;
     else if (p == kParamOffset)
         pThis->dtc->outputOffset = pThis->v[kParamOffset] * 0.001f;
+    else if (p == kParamPhaseOffset)
+        pThis->dtc->phaseOffset = pThis->v[kParamPhaseOffset] * 0.01f;
+    else if (p == kParamSpeed)
+        pThis->dtc->speedMultiplier = speedMultiplierForIndex(pThis->v[kParamSpeed]);
+    else if (p == kParamClockIn)
+        resetClock(pThis->dtc);
     else if (p == kParamEquation && pThis->v[kParamEquation] == mathulate::kEquationLorenz)
         mathulate::resetState(&pThis->dtc->core);
 }
@@ -122,20 +204,48 @@ static void step(_NT_algorithm* self, float* busFrames, int numFramesBy4) {
     int inputAIndex = pThis->v[kParamInputA] - 1;
     int inputBIndex = pThis->v[kParamInputB] - 1;
     int outputIndex = pThis->v[kParamOutput] - 1;
+    int clockIndex = pThis->v[kParamClockIn] - 1;
     if (inputAIndex < 0 || inputBIndex < 0 || outputIndex < 0)
         return;
 
     const float* inputA = busFrames + inputAIndex * numFrames;
     const float* inputB = busFrames + inputBIndex * numFrames;
+    const float* clockIn = clockIndex >= 0 ? busFrames + clockIndex * numFrames : NULL;
     float* output = busFrames + outputIndex * numFrames;
     bool replace = pThis->v[kParamOutputMode];
     int equation = pThis->v[kParamEquation];
     float scale = dtc->outputScale;
     float offset = dtc->outputOffset;
+    float sr = sampleRate();
+    float internalPeriod = sr / clampf(dtc->speedMultiplier, 0.001f, 64.0f);
 
     for (int i = 0; i < numFrames; ++i) {
-        float volts = mathulate::processSample(&dtc->core, equation, inputA[i], inputB[i], scale, offset);
+        float period = internalPeriod;
+
+        if (clockIn) {
+            bool clockHigh = clockIn[i] > kClockThreshold;
+            bool rising = clockHigh && !dtc->previousClockHigh;
+            if (rising) {
+                if (dtc->clockSeen && dtc->samplesSinceClock > 0)
+                    dtc->clockPeriodSamples = (float)dtc->samplesSinceClock;
+                dtc->samplesSinceClock = 0;
+                dtc->clockSeen = true;
+                dtc->phase = 0.0f;
+                mathulate::resetState(&dtc->core);
+            }
+            dtc->previousClockHigh = clockHigh;
+            if (dtc->clockSeen)
+                period = dtc->clockPeriodSamples / clampf(dtc->speedMultiplier, 0.001f, 64.0f);
+            ++dtc->samplesSinceClock;
+        }
+
+        period = clampf(period, 1.0f, sr * 3600.0f);
+        float effectivePhase = mathulate::wrapPhase(dtc->phase + dtc->phaseOffset + mathulate::normalizeCv(inputA[i]) * 0.5f);
+        float volts = mathulate::processPhaseSample(&dtc->core, equation, dtc->phase, dtc->phaseOffset, inputA[i], inputB[i], scale, offset);
         output[i] = replace ? volts : (output[i] + volts);
+        dtc->displayPhase = effectivePhase;
+        dtc->displayOutput = volts;
+        dtc->phase = mathulate::wrapPhase(dtc->phase + 1.0f / period);
     }
 }
 
